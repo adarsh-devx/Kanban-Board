4830b9c4f970b3a195ff0dde833342b58a08a74a
adarsh-devx <devx.adarshh@gmail.com>
2026-06-19 15:30:26 +0530

    Kanban board

---
+++ script.js
@@ -0,0 +1,47 @@
+const todo = document.querySelector("#todo");
+const progress = document.querySelector("#progress");
+const done = document.querySelector("#done");
+
+let dragElement = null
+
+const task = document.querySelectorAll(".task");
+
+task.forEach((task) => {
+  task.addEventListener("drag", (e) => {
+    dragElement = task
+  });
+});
+
+
+
+
+
+
+function addDragEventsOnColumn(column) {
+  column.addEventListener("dragenter", (e) => {
+    e.preventDefault();
+    column.classList.add("hover-over");
+  });
+
+  column.addEventListener("dragleave", (e) => {
+    e.preventDefault();
+    column.classList.remove("hover-over");
+  });
+
+
+  column.addEventListener("dragover", (e) => {
+    e.preventDefault();
+  })
+
+
+  column.addEventListener("drop", (e) => {
+    e.preventDefault();
+    column.appendChild(dragElement)
+    column.classList.remove("hover-over")
+
+  })
+}
+
+addDragEventsOnColumn(todo)
+addDragEventsOnColumn(progress)
+addDragEventsOnColumn(done)
